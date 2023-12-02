f928dea562c6ad30dd4606f6182e14df1bd82be7
arne-p <78234073+pk-devs@users.noreply.github.com>
2023-12-03 00:40:49 +0100

    added _redirects

--- src/components/movies/Movie.jsx
+++ src/components/movies/Movie.jsx
@@ -4,15 +4,12 @@ import { Link } from "react-router-dom"
 
 export const Movie = () => {
   const { id } = useParams()
-  console.log(id)
   const [movieDetails, setMovieDetails] = useState(null);
   const apiKey = "ea6a347864d3baf0357cebd9eb28097c"
-
-
+  
   useEffect(() => {
     
     const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
-    console.log(url)
 
     fetch(url)
         .then(response => {
@@ -38,12 +35,10 @@ export const Movie = () => {
          <Link to="/"> <button>Back to movies</button> </Link>
           <div className="movie-content">
               <img src={posterUrl} alt={movieDetails.title} className="movie-poster" />
-              
               <div className="movie-info">
                   <h1>{movieDetails.title}</h1>
                   <p><strong>Rating: {movieDetails.vote_average.toFixed(1)} / 10</strong></p>
                   <p>{movieDetails.overview}</p>
-                  
               </div>
           </div>
       </div>
